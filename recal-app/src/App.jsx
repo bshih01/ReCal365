9b792f6041ab05d980dc8ae8d40d7fa6d3238054
Brandon Shih <branshih10@gmail.com>
2024-12-09 16:05:34 -0500

    dark mode

--- recal-app/src/App.jsx
+++ recal-app/src/App.jsx
@@ -3,14 +3,25 @@ import HomePage from './components/HomePage';
 import CalendarPage from './components/CalendarPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+          </Routes>
+      </Router>
+      </ThemeProvider>
   );
 };
 
